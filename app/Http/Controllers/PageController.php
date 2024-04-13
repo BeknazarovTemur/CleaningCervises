a7ba8f2286e863c7337df3f23f273c764890f0f1
BeknazarovTemur <imtemurb@gmail.com>
2024-04-13 16:48:34 +0500

    Made routes and controller, separated blade files

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function main()
+    {
+        return view('main');
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
+
+    public function service()
+    {
+        return view('service');
+    }
+
+    public function project()
+    {
+        return view('project');
+    }
+
+    public function contact()
+    {
+        return view('contact');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PageController::class, 'main'])->name('main');
+Route::get('about', [PageController::class, 'about'])->name('about');
+Route::get('service', [PageController::class, 'service'])->name('service');
+Route::get('project', [PageController::class, 'project'])->name('project');
+Route::get('contact', [PageController::class, 'contact'])->name('contact');
+
